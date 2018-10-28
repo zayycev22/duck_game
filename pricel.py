f2f95604029eb676c27ec9941d88ba9aa2ec2097
Даниил Сурков <7535415@gmail.com>
2018-10-28 19:47:59 +0300

    Update pricel.py

--- pricel.py
+++ pricel.py
@@ -1,9 +1,23 @@
+import os
 import pygame
+import random
+import time
+from pygame import *
+import threading
+import duck
 
 pygame.init()
 
-done = False
 
+class Pricel:
+
+    def __init__(self, pos):
+
+        self.image = pygame.image.load(os.path.join("Images", "pr.png"))
+        self.image = transform.scale(self.image, (20, 20))
+
+
+done = False
 while not done:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
@@ -11,9 +25,9 @@ while not done:
         if i.type == pygame.KEYDOWN:
             if i.button == 3:
                 if i.pos == duck.pos:
-                    duck
+                    duck.delete()
                     pygame.display.update()
                 elif i.pos == duck_rev.pos:
-                duck_rev
-            pygame.display.update()
+                    duck_rev.delete()
+                    pygame.display.update()
     pygame.time.delay(20)
