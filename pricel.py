55b31dc91079dcca3ac8a9546fe32aa9e993108e
Даниил Сурков <7535415@gmail.com>
2018-11-14 09:20:03 +0300

    Replace pricel.py

--- pricel.py
+++ pricel.py
@@ -5,27 +5,25 @@ import time
 from pygame import *
 import threading
 
-
 import move
+
 pygame.init()
-pic1="Images/scope.png"
+pic1 = "Images/scope.png"
+
 
 class Pricel:
 
     def __init__(self, pos):
-
         self.image = pygame.image.load(os.path.join(pic1))
         self.image = transform.scale(self.image, (40, 40))
-        self.x=pos[0]
-        self.y=pos[1]
+        self.x = pos[0]
+        self.y = pos[1]
 
     def update(self):
         pos = pygame.mouse.get_pos()
-        #self.x += 10
-        #self.y += 10
-        self.x = pos[0]-20
-        self.y = pos[1]-20
+        # self.x += 10
+        # self.y += 10
+        self.x = pos[0] - 20
+        self.y = pos[1] - 20
 #    def handle_mouse_move(self,pos):
-        #if self.bounds.collidepoint(pos)
-
-
+# if self.bounds.collidepoint(pos)
